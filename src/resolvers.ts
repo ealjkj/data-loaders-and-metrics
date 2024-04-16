a1ac400cd957179959d07d8fad1341d9ef898d83
intekglobal02 <intekglobal02@intekgll02sMini.attlocal.net>
2024-04-16 16:03:55 -0700

    initial changes

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,36 @@
-import models from "./db/models";
+// import models from "./db/models";
+import asyncModels from "./db/models/ModelWrapper";
+import { Post, PostComment, User } from "./types";
+const models = asyncModels;
 
 const resolvers = {
+  User: {
+    async posts(user: User) {
+      return models.Post.findMany({ author: user.id });
+    },
+    async comments(user: User) {
+      return models.Comment.findMany({ author: user.id });
+    },
+  },
+  Post: {
+    async author(post: Post) {
+      return models.User.findOne({ id: post.author });
+    },
+    async comments(post: Post) {
+      return models.Comment.findMany({ post: post.id });
+    },
+  },
+  Comment: {
+    async author(comment: PostComment) {
+      return models.User.findOne({ id: comment.author });
+    },
+    async post(comment: PostComment) {
+      return models.Post.findOne({ id: comment.post });
+    },
+  },
+
   Query: {
-    user() {
+    async user() {
       return models.User.findOne({});
     },
   },
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,6 +1,25 @@
 const typeDefs = `#graphql
     type User {
         id: ID!
+        username: String!
+        email: String
+        posts: [Post]!
+        comments: [Comment]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        author: User!
+        comments: [Comment]!
+    }
+
+    type Comment {
+        id: ID!
+        content: String!
+        author: User!
+        post: Post!
     }
 
     type Query {
